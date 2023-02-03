e577607572c08f1e7886f9fb550f5b23b3f887a1
Jixun Wu <jixun.moe@gmail.com>
2023-02-03 00:53:31 +0000

    fix: first 16 bytes were incorrectly encrypted.

--- src/tc_tea/tc_tea_cbc.cpp
+++ src/tc_tea/tc_tea_cbc.cpp
@@ -122,8 +122,11 @@ bool CBC_Encrypt(uint8_t *cipher, size_t *p_cipher_len, const uint8_t *plain, si
     size_t pad_len = CBC_GetPaddingSize(plain_len);
     size_t header_len = 1 + pad_len + TEA_SALT_SIZE;
 
-    GenerateRandomBytes(cipher, header_len);
+    // Clear the first 2 blocks.
+    std::fill_n(cipher, kTeaBlockSize * 2, 0);
 
+    // Begin header generation
+    GenerateRandomBytes(cipher, header_len);
     cipher[0] = (cipher[0] & uint8_t{0b1111'1000})             // NOLINT(*-magic-numbers)
                 | static_cast<uint8_t>(pad_len & 0b0000'0111); // NOLINT(*-magic-numbers)
 
@@ -131,30 +134,29 @@ bool CBC_Encrypt(uint8_t *cipher, size_t *p_cipher_len, const uint8_t *plain, si
     std::array<uint8_t, kTeaBlockSize> next_iv2{};
 
     auto *p_cipher = cipher;
-    const auto *p_plain = plain + kTeaBlockSize;
-    const auto *p_plain_end = plain + plain_len;
+    const auto *p_plain = plain;
+    const auto *p_plain_end = plain + plain_len - kTeaBlockSize; // we need to stop a block before that
 
     auto tea_encrypt_next_block = [&](const uint8_t *p_src) {
         // XOR previous cipher block
         XorTeaBlock(p_cipher, p_src, &p_cipher[-kTeaBlockSize]);
 
-        // store iv2
+        // store iv2 for next iteration
         std::copy_n(p_cipher, kTeaBlockSize, next_iv2.begin());
 
         // TEA ECB
         ECB_EncryptBlock(p_cipher, key_be.data());
 
-        // XOR iv2
+        // XOR iv2 from previous iteration
         XorTeaBlock(p_cipher, iv2.data());
 
         iv2 = next_iv2;
         p_cipher += kTeaBlockSize;
     };
 
-    // Prepare for first 2 blocks
+    // Copy required plain-text to the temp buffer.
     size_t initial_plain_process_len = std::min(kTeaBlockSize * 2 - header_len, plain_len);
     std::copy_n(plain, initial_plain_process_len, &cipher[header_len]);
-    std::fill_n(&cipher[initial_plain_process_len], kTeaBlockSize * 2 - initial_plain_process_len, 0);
     p_plain += initial_plain_process_len;
 
     // Preserve iv2
@@ -162,18 +164,20 @@ bool CBC_Encrypt(uint8_t *cipher, size_t *p_cipher_len, const uint8_t *plain, si
     ECB_EncryptBlock(p_cipher, key_be.data());
     p_cipher += kTeaBlockSize;
 
+    // Decrypt second block
     tea_encrypt_next_block(p_cipher);
 
+    // Check if there're any more data to encrypt after first 2 blocks.
     if (cipher_len > kTeaBlockSize * 2)
     {
         while (p_plain < p_plain_end)
         {
-            tea_encrypt_next_block(&p_plain[-kTeaBlockSize]);
+            tea_encrypt_next_block(p_plain);
             p_plain += kTeaBlockSize;
         }
 
         std::array<uint8_t, kTeaBlockSize> last_block{};
-        last_block[0] = plain[plain_len - 1];
+        std::copy_n(p_plain, kTeaBlockSize - TEA_PADDING_ZERO_SIZE, last_block.begin());
         tea_encrypt_next_block(last_block.data());
     }
 
